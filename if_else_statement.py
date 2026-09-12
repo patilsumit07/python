1244290fdbc49ce49b1ce5625144c353e2129711
Sumit Patil <sumitpatil12309@gmail.com>
2026-09-12 20:09:43 +0530

    Update if_else_statement.py

--- if_else_statement.py
+++ if_else_statement.py
@@ -1,4 +1,4 @@
-# Largest three number:
+# 1) Largest three number:
 a=int(input("Enter a first number :"))
 b=int(input("Enter a second number :"))
 c=int(input("Enter a third number :"))
@@ -18,3 +18,17 @@ else:
 # Enter a second number :99
 # Enter a third number :68
 # larger number= 99
+
+# 2) Check two number's are equal or not using function :
+def check(a,b):
+    if a==b:
+        print("The given both number's are equal")
+    else:
+        print("The given number's are not equal ")
+X=int(input("Enter a first number :"))
+Y=int(input("Enter a second number :"))
+check(X,Y)
+# Output:
+# Enter a first number :7
+# Enter a second number :8
+# The given number's are not equal 
