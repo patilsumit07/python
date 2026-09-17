da8acdd12ff4436845d103f89a92da49367dbdec
Sumit Patil <sumitpatil12309@gmail.com>
2026-09-17 19:35:16 +0530

    Update if_else_statement.py

--- if_else_statement.py
+++ if_else_statement.py
@@ -52,4 +52,27 @@ else:
 # Output:-
 # Enter a Year :2025
 # Not Leap Year
- 
+
+# 4)Prime number in given range :-
+start = int(input("Enter a Start Number: "))
+end = int(input("Enter a End Number: "))
+
+print("Prime Numbers:")
+
+for num in range(start, end + 1):
+
+    if num > 1:
+        for i in range(2, num):
+            if num % i == 0:
+                break
+        else:
+            print(num)   
+
+# Output :-
+# Enter a Start Number: 2
+# Enter a End Number: 9
+# Prime Numbers:
+# 2
+# 3
+# 5
+# 7
