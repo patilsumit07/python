0a0454f65f411d6d5285197c28f7adfbc8cd94a4
Sumit Patil <sumitpatil12309@gmail.com>
2026-09-18 18:15:15 +0530

    Create for loop.py

---
+++ for loop.py
@@ -0,0 +1,15 @@
+# Fibonacci sequence up to n terms :-
+n=int(input("Enter a terms :"))
+a=0
+b=1
+print("Fibonacci Series")
+for i in range(n):
+    print(a,end ="\t")
+    c=a+b
+    a=b
+    b=c
+
+# Output :-
+# Enter a terms :5
+# Fibonacci Series
+# 0       1       1       2       3
