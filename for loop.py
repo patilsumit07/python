bd3010f96061fbf800bead2c08440f42e698a61e
Sumit Patil <sumitpatil12309@gmail.com>
2026-09-19 19:23:09 +0530

    Update for loop.py

--- for loop.py
+++ for loop.py
@@ -14,9 +14,24 @@ for i in range(n):
 # Fibonacci Series
 # 0       1       1       2       3
 
+
 # Muliplication table from 1 to 10 :-
 for i in range(1,11):
     print("\nTable of",i)
 
     for j in range(1,11):
         print(i,"x",j,"=",i*j)
+
+# Output :-
+# Table of 1
+# 1 x 1 = 1
+# 1 x 2 = 2
+# 1 x 3 = 3
+# 1 x 4 = 4
+# 1 x 5 = 5
+# 1 x 6 = 6
+# 1 x 7 = 7
+# 1 x 8 = 8
+# 1 x 9 = 9
+# 1 x 10 = 10
+# like this table 1 to 10     
